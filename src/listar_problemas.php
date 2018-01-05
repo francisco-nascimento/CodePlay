99cf9aafcb276b2354010857097e564fb697495f
oseiasf <oseiasf25@gmail.com>
2018-01-05 12:23:43 -0300

    Alterações necessarias

--- src/listar_problemas.php
+++ src/listar_problemas.php
@@ -4,13 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <title>Problemas</title>
-  
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-
-  
-      <link rel="stylesheet" href="css/style.css">
-
-  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -29,20 +24,23 @@
          <th>Professor</th>
          <th>Questões</th>
          <th>Classificação</th>
+         <th>Opções</th>
       </tr>
    <?php
 
   require 'conexao.php';
 
-   $resultado = $conexao->query("select profe.nome, proble.desc_Problema, proble.classificacao from Professor as profe left join Problema as proble on profe.id = proble.id_Professor");
+   $resultado = $conexao->query("select profe.nome, proble.desc_Problema, proble.classificacao from Professor as profe left join Problema as proble on profe.id = proble.id_Professor limit 100");
 
   foreach($resultado as $linha){ 
 
 ?>
       <tr>
-         <?="<td>".$linha['nome']."</td> ";?>
-         <?="<td><a href=''>".$linha['desc_Problema']."</a></td> ";?>
-         <?="<td>".$linha['classificacao']."</td> ";}?>
+         <?="<td>".$linha['nome']."</td>";?>
+         <?="<td><a href=''>".$linha['desc_Problema']."</a></td>";?>
+         <?="<td>".$linha['classificacao']."</td>";?>
+         <td><button><a href=''>Editar</a></button> | <button><a href=''>Liberar</a> </button> </td>
+<?php } ?>
       </tr>
    </table>
 </div>
