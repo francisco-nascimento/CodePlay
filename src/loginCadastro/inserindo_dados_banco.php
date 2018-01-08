8aba584531f48dfb310008d517b635a7ee6b8bb4
oseiasf <oseiasf25@gmail.com>
2018-01-08 15:17:48 -0300

    Só identei

--- src/loginCadastro/inserindo_dados_banco.php
+++ src/loginCadastro/inserindo_dados_banco.php
@@ -1,60 +1,53 @@
 <?php 
 
 	try{
-
 			
-			$conexao = new PDO('mysql:host=localhost;dbname=Codeplay', "root", "@luno1fpe");
+		$conexao = new PDO('mysql:host=localhost;dbname=Codeplay', "root", "@luno1fpe");
+		$conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-			$conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		$matricula = $_POST["matricula"];
+		$nome = $_POST["nome"];
+		$senha = $_POST["senha"];
+		$email = $_POST["email"];
+		$situacao = 1;
 
-			$matricula = $_POST["matricula"];
-			$nome = $_POST["nome"];
-			$senha = $_POST["senha"];
-			$email = $_POST["email"];
-			$situacao = 1;
-
-
-
-			$inserirAluno = "insert into Aluno (matricula, nome, senha, email, situacao) values (?,?,?,?,?)";
-		
-			$inserirProfessor = "insert into Professor (matricula, nome, senha, email, situacao) values (?,?,?,?,?)";
+		$inserirAluno = "insert into Aluno (matricula, nome, senha, email, situacao) values (?,?,?,?,?)";
+		$inserirProfessor = "insert into Professor (matricula, nome, senha, email, situacao) values (?,?,?,?,?)";
 			
-			if($_POST['tipoUsuario'] == 1){
+		if($_POST['tipoUsuario'] == 1){
 
-				$stmt = $conexao->prepare($inserirProfessor);
-				session_start();
-				$_SESSION["nome"] = $nome;
-				$senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
-				$_SESSION["email"] = $email;
+			$stmt = $conexao->prepare($inserirProfessor);
+			session_start();
+			$_SESSION["nome"] = $nome;
+			$senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
+			$_SESSION["email"] = $email;
 
-				$stmt->bindValue(1, $matricula);
-				$stmt->bindValue(2, $nome);
-				$stmt->bindValue(3, $senhaCriptografada);
-				$stmt->bindValue(4, $email);
-				$stmt->bindValue(5, $situacao);
+			$stmt->bindValue(1, $matricula);
+			$stmt->bindValue(2, $nome);
+			$stmt->bindValue(3, $senhaCriptografada);
+			$stmt->bindValue(4, $email);
+			$stmt->bindValue(5, $situacao);
 
-				$stmt->execute();
+			$stmt->execute();
 
-				header("Location: loginCadastro.php");
+			header("Location: loginCadastro.php");
 
 
-			}else{
-				$stmt = $conexao->prepare($inserirAluno);
-			
-			
-				//session_start();
-				$_SESSION["nome"] = $nome;
-				$senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
-				$_SESSION["email"] = $email;
+		}else{
+			$stmt = $conexao->prepare($inserirAluno);
+			//session_start();
+			$_SESSION["nome"] = $nome;
+			$senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
+			$_SESSION["email"] = $email;
 				
-				$stmt->bindValue(1, $matricula);
-				$stmt->bindValue(2, $nome);
-				$stmt->bindValue(3, $senhaCriptografada);
-				$stmt->bindValue(4, $email);
+			$stmt->bindValue(1, $matricula);
+			$stmt->bindValue(2, $nome);
+			$stmt->bindValue(3, $senhaCriptografada);
+			$stmt->bindValue(4, $email);
 				
-				$stmt->execute();
+			$stmt->execute();
 
-				header("Location: loginCadastro.php");
+			header("Location: loginCadastro.php");
 
 			}
 
@@ -62,11 +55,4 @@
 
 	}catch(PDOException $e){
 			echo $e->getMessage();
-		}
-
-
-
-
-// var_dump($matricula);
-
 ?>
--- src/loginCadastro/loginCadastro.php
+++ src/loginCadastro/loginCadastro.php
@@ -1,125 +1,79 @@
 <!DOCTYPE html>
 <html >
-<head>
+  <head>
   <meta charset="UTF-8">
   <title> Login </title>
-  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
   <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-  
   <script type="text/javascript">
+      
+    function validaSenha(){
 
+       var senha = document.getElementById("senha")
+       var confirmaSenha = document.getElementById("confirmaSenha");
 
-        function validaSenha(){
+       if(senha.value != confirmaSenha.value) {
+         alert("Senhas diferentes, por favor insira as senha iguais!");
+         confirmaSenha.focus();
+         return false;
+       }
 
-          var senha = document.getElementById("senha")
-          var confirmaSenha = document.getElementById("confirmaSenha");
-
-          if(senha.value != confirmaSenha.value) {
-            alert("Senhas diferentes, por favor insira as senha iguais!");
-              confirmaSenha.focus();
-                return false;
-          }
-
-                         
-        }
-          
-    </script>
-    <?php
-
-          if (isset($_SESSION["USUARIO_LOGADO"])) {
-    # code...
-             header("Location: /index.php");
-          }
-
-            include '../verifica.php';
-
+                           
+    }
             
+  </script>
+  <?php
 
-    ?>
- 
-</head>
-
-<body>
-
- <?php
-  
-  include '../imports.php';
-
-?>
-  <br><br>
-  
-<div class="container">
-  <div class="info">
-    <h1> Login </h1><span>Acesse sua conta</span>
-  </div>
-</div>
-<div class="form">
-  <div class="thumbnail"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/hat.svg"/></div>
-  <form action="inserindo_dados_banco.php" method="POST" onsubmit="return validaSenha();" class="register-form" >
+    if (isset($_SESSION["USUARIO_LOGADO"])) {
+      header("Location: /index.php");
+    }
+    include '../verifica.php';
+  ?>
    
-  
-    <label>
-      Insira sua matricula
-    </label>
-    <input type="text" required autocomplete="off"  placeholder="XXX2016" name="matricula">
-     <label>
-      Insira seu nome
-    </label>
-    <input type="text" required autocomplete="off"  placeholder="Fulano de Tal" name="nome">
-    <label>
-      Insira uma senha
-    </label>
-    <input type="password" required autocomplete="off" id="senha" placeholder="********" name="senha">
-    <label>
-      Repita a senha
-    </label>
-    <input type="password" required autocomplete="off" id="confirmaSenha" placeholder="********">
-    <label>
-      Insira um email
-    </label>
-    <input type="text" required autocomplete="off" placeholder="fulano@gmail.com" name="email">
+  </head>
 
-	<label> Professor</label>
-	<input type="radio" name="tipoUsuario" required="required" value="1"> <br>
-	
-	<label>Aluno</label>
-	<input type="radio" name="tipoUsuario"  required="required" value="0"> <br>
-
-    <button>criar</button>
-    <p class="message">Já registrado? <a href="#">Sign In</a></p>
-	
-
-
-  </form>
-  <form action="/loginCadastro/verificaLogin.php" method="POST" class="login-form">
-  	<label>
-       Email 
-    </label>
-    <input type="text" name="email" required placeholder="fulano@gmail.com">
-    <label>
-      Senha
-    </label>
-    <input type="password" name="senha" required placeholder="********"><br>
-
-	<label> Professor</label>
-	<input type="radio" name="tipoUsuario" required value="1"> <br>
-	
-	<label>Aluno</label>
-	<input type="radio" name="tipoUsuario" value="0"> <br>
-
-    <button>login</button>
-    <p class="message">Não registrado? <a href="#">Crie uma conta</a></p>
-  </form>
-
-
-</div>
-
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
+  <body>
+    <div class="container">
+      <div class="info">
+        <h1> Login </h1><span>Acesse sua conta</span>
+      </div>
+    </div>
+    <div class="form">
+      <div class="thumbnail"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/hat.svg"/></div>
+        <form action="inserindo_dados_banco.php" method="POST" onsubmit="return validaSenha();" class="register-form" >
+          <label>Insira sua matricula</label>
+          <input type="text" required autocomplete="off"  placeholder="XXX2016" name="matricula">
+          <label>Insira seu nome</label>
+          <input type="text" required autocomplete="off"  placeholder="Fulano de Tal" name="nome">
+          <label>Insira uma senha</label>
+          <input type="password" required autocomplete="off" id="senha" placeholder="********" name="senha">
+          <label>Repita a senha</label>
+          <input type="password" required autocomplete="off" id="confirmaSenha" placeholder="********">
+          <label>Insira um email</label>
+          <input type="text" required autocomplete="off" placeholder="fulano@gmail.com" name="email">
+          <label> Professor</label>
+          <input type="radio" name="tipoUsuario" required="required" value="1"> <br>
+          <label>Aluno</label>
+          <input type="radio" name="tipoUsuario"  required="required" value="0"> <br>
+          <button>criar</button>
+          <p class="message">Já registrado? <a href="#">Sign In</a></p>
+        </form>
+        <form action="/loginCadastro/verificaLogin.php" method="POST" class="login-form">
+            <label>Email</label>
+            <input type="text" name="email" required placeholder="fulano@gmail.com">
+            <label>Senha</label>
+            <input type="password" name="senha" required placeholder="********"><br>
+            <label> Professor</label>
+            <input type="radio" name="tipoUsuario" required value="1"> <br>
+            <label>Aluno</label>
+            <input type="radio" name="tipoUsuario" value="0"> <br>
+            <button>login</button>
+            <p class="message">Não registrado? <a href="#">Crie uma conta</a></p>
+        </form>
+    </div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script  src="../js/index.js"></script>
-
-</body>
+  </body>
 </html>
