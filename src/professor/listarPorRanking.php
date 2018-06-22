b9dac5370e5c4ff247d3ea4580931ca9ab85c278
Francisco do Nascimento Junior <chico.nascimento@gmail.com>
2018-06-22 01:20:02 -0300

    Filtrar por turma no ranking + Ajuste na expiracao da sessao

--- src/professor/listarPorRanking.php
+++ src/professor/listarPorRanking.php
@@ -1,5 +1,25 @@
 <?php 
   require ($_SERVER["DOCUMENT_ROOT"].'/verifica.php');
+  require ($_SERVER["DOCUMENT_ROOT"].'/conexao.php');
+  require ($_SERVER["DOCUMENT_ROOT"].'/aluno/areaAluno.php');
+  require ($_SERVER["DOCUMENT_ROOT"].'/aluno/AreaAlunoDAO.php');
+
+  function gerarSelectTurmas($con, $selecao){
+    $turmaDAO = new TurmaDAO($con);
+    $turmas = $turmaDAO->listAll();
+    $html_select = "<SELECT name ='pesq-turma' id='pesq-turma' readonly=true>";
+    $html_select .= "<option value=''>Todas as turmas</option>";
+    foreach($turmas as $turma){
+      $idTurma = $turma->id;
+      $nomeTurma = $turma->desc_Turma;
+      $nomeProfessor = $turma->nome;
+      $selected = $idTurma === $selecao ? 'selected' : '';
+      $html_select .= "<option value='$idTurma' ". $selected . ">$nomeTurma ($nomeProfessor)</option>";
+    }
+    $html_select .= "</select>";
+    return $html_select;
+  }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,21 +31,37 @@
   </head>
   <body>
     <br><br><br>
-    <div class="table-users">
+    <div class="table-ranking">
       <div class="header">Ranking</div>
+      <fieldset>
+      <div>
+        <form method="POST" action="">
+          Turma: <?=gerarSelectTurmas($conexao, $_POST['pesq-turma']);?>
+          <button type="submit">Filtrar</button>
+        </form>
+      </div>        
+      </fieldset>
         <table cellspacing="0">
-          <tr>
-            <th>Aluno</th>
-            <th>Pontuação</th>
+          <tr class="title2">
+            <td>Aluno</td>
+            <td>Pontuação</td>
           </tr>
           <?php
-            require ($_SERVER["DOCUMENT_ROOT"].'/conexao.php');
-            $resultado = $conexao->prepare("select nome, pontuacao from Aluno order by pontuacao desc, nome limit 50");
+
+            if (isset($_POST['pesq-turma'])){
+              $id_turma = $_POST['pesq-turma'];
+            }
+            if (intval($id_turma) > 0){
+              $resultado = $conexao->prepare("select nome, pontuacao from Aluno where id_turma = " . $id_turma . " order by pontuacao desc, nome limit 50");              
+            } else {
+              $resultado = $conexao->prepare("select nome, pontuacao from Aluno order by pontuacao desc, nome limit 50");
+            }
             $resultado->execute();
+            $pos = 1;
             foreach($resultado as $linha){ 
           ?>
           <tr>
-            <td><?=$linha['nome'];?></td>
+            <td><?=$pos++ . '. ' . $linha['nome'];?></td>
             <td>
               <?php
                 $pontuacao = $linha['pontuacao'];
--- src/verifica.php
+++ src/verifica.php
@@ -1,5 +1,6 @@
 <?php
 
+	const LIMITE_SESSION_TIME = 1200;
 	session_start();
 		
 	 include 'conexao.php';
@@ -9,6 +10,16 @@
 		require ($_SERVER["DOCUMENT_ROOT"].'/navegacao.php');
 		
 	}else{
+
+		// Verifica a expiracao da sessao
+		if (isset($_SESSION['ultima_atividade']) && (time() - $_SESSION['ultima_atividade'] > LIMITE_SESSION_TIME)) {
+
+		    // última atividade foi mais de 60 minutos atrás
+		    session_unset();     // unset $_SESSION  
+		    session_destroy();   // destroindo session data 
+		    header("Location: /index.php");
+		}
+		$_SESSION['ultima_atividade'] = time(); // update da ultima atividade
 
 		$usuario = $_SESSION['USUARIO_LOGADO'];
 
